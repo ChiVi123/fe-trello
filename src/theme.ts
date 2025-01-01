0516a2703508885f62ff5b0dfc894f363fd7258a
ChiVi123 <nguyenhoangchivi@gmail.com>
2025-01-01 15:33:59 +0700

    create basic layout

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,33 @@
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
+declare module '@mui/material/styles' {
+    interface Theme {
+        trello: {
+            appBarHeight: string;
+            boardBarHeight: string;
+        };
+    }
+    // allow configuration using `createTheme()`
+    interface ThemeOptions {
+        trello?: {
+            appBarHeight?: string;
+            boardBarHeight?: string;
+        };
+    }
+}
+
 const theme = createTheme({
-    colorSchemes: { light: true, dark: true },
     cssVariables: {
         colorSchemeSelector: 'class',
+    },
+    colorSchemes: {
+        light: { palette: { primary: teal, secondary: deepOrange } },
+        dark: { palette: { primary: cyan, secondary: orange } },
+    },
+    trello: {
+        appBarHeight: '48px',
+        boardBarHeight: '58px',
     },
 });
 
