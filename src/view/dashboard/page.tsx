0c0efffaec18fa1a988501290cd186731c5764eb
ChiVi123 <nguyenhoangchivi@gmail.com>
2025-01-02 20:30:12 +0700

    add new column component

--- src/view/dashboard/page.tsx
+++ src/view/dashboard/page.tsx
@@ -1,5 +1,7 @@
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
 import Box from '@mui/material/Box';
-import Column from './component/column';
+import Button from '@mui/material/Button';
+import Column from './components/column';
 
 function DashboardPage() {
     return (
@@ -17,8 +19,25 @@ function DashboardPage() {
             <Column />
             <Column />
             <Column />
-            <Column />
-            <Column />
+
+            <Box
+                sx={{
+                    minWidth: '200px',
+                    maxWidth: '200px',
+                    height: 'fit-content',
+                    mx: 2,
+                    borderRadius: '6px',
+                    bgcolor: '#ffffff3d',
+                }}
+            >
+                <Button
+                    fullWidth
+                    startIcon={<NoteAddIcon />}
+                    sx={{ justifyContent: 'flex-start', pl: 2.5, py: 1, color: 'white' }}
+                >
+                    Add new column
+                </Button>
+            </Box>
         </Box>
     );
 }
