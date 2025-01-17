eb3cf16c4ca560d0958cfe55fb92087b6622107d
ChiVi123 <nguyenhoangchivi@gmail.com>
2025-01-17 08:28:58 +0700

    handle toast and loading-api

--- src/shared/utils/formatters.ts
+++ src/shared/utils/formatters.ts
@@ -11,3 +11,20 @@ export const generatePlaceholderCard = (column: IColumnEntity): ICardEntity => (
     columnId: column._id,
     FE_PlaceholderCard: true,
 });
+
+// css pointer-event prevent user spam click call api
+// With Axios Interceptors handle a whole project
+// Guide: add class "interceptor-loading" for elements.
+export const interceptorLoadingElements = (calling: boolean) => {
+    const elements = document.querySelectorAll('.interceptor-loading') as NodeListOf<HTMLElement>;
+    elements.forEach((element) => {
+        if (calling) {
+            // if call API pending (calling === true)
+            element.style.opacity = '0.5';
+            element.style.pointerEvents = 'none';
+        } else {
+            element.style.opacity = 'initial';
+            element.style.pointerEvents = 'initial';
+        }
+    });
+};
